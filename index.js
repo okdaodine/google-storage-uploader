9db12c88f34f1b997fab3de5b114aef4dc5cbbb4
okdaodine <btc.link.grt@gmail.com>
2023-05-04 13:26:01 +0800

    tweaks

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Storage } = require('@google-cloud/storage');
+const argv = require('yargs').argv;
+
+const { src, dest, bucketName } = argv;
+
+if (!src) {
+  console.log('Error: src is required');
+  return;
+}
+
+if (!dest) {
+  console.log('Error: dest is required');
+  return;
+}
+
+if (!bucketName) {
+  console.log('Error: bucketName is required');
+  return;
+}
+
+// Initialize storage
+const storage = new Storage({
+  keyFilename: `./key.json`,
+})
+
+const bucket = storage.bucket(bucketName)
+
+// Sending the upload request
+bucket.upload(
+  src,
+  {
+    destination: dest.endsWith('/') ? `${dest}${src.split('/').pop()}` : dest,
+  },
+  function (err) {
+    if (err) {
+      console.error(`Error uploading ${src}: ${err}`)
+    } else {
+      console.log(`${src} uploaded to ${bucketName}.`)
+    }
+  }
+)
+
